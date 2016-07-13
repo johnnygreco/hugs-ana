8b470003046ed5faed991c0bb5b4db7f6f9ba6a4
Johnny Greco <jgreco@astro.princeton.edu>
2016-07-13 12:22:46 -0400

    setting param file now works

--- param_tests.py
+++ param_tests.py
@@ -6,6 +6,8 @@ from toolbox.utils import ds9view
 imfile = 'deepCoadd_HSC-I_9348_8-6.fits'
 
 config = {'PARAMETERS_NAME' : 'params',
+          'DEBLEND_NTHRESH' : 32,
+          'DEBLEND_MINCONT' : 1,
           'DETECT_MINAREA'  : 5,
           'DETECT_THRESH'   : 1.5, 
           'ANALYSIS_THRESH' : 1.5}
