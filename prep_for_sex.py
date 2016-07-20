3c7ffab77ed9e832c230ca914fe2ffadb2faedfb
Johnny Greco <jgreco@astro.princeton.edu>
2016-07-20 16:29:48 -0400

    script prep images for sextractor

---
+++ prep_for_sex.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+
+"""
+Prepare images for SExtractor.
+"""
+
+from __future__ import print_function
+
+import os
+from utils import prep_for_sex
+
+imdir = 'sexin/deepCoadds/HSC-I'
+
+tracts = os.listdir(imdir)
+
+for t in tracts:
+    patches = os.listdir(os.path.join(imdir, t))
+    for p in patches:
+        prep_for_sex(t, p)
