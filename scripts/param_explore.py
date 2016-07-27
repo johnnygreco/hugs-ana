21c99b06465bb46c5de9e6a93702732ad0c60e3e
Johnny Greco <jgreco@astro.princeton.edu>
2016-07-27 12:55:03 -0400

    explore params with new sexpy

--- scripts/param_explore.py
+++ scripts/param_explore.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python 
 
 """
-Explore the influence of the SExtractor 
-configuration parameters.
+Explore the influence of the SExtractor configuration parameters.
 """
 
 from __future__ import print_function 
@@ -14,18 +13,55 @@ def explore(tract, patch, band='I'):
 
     relpath = 'HSC-'+band+'/'+tract+'/'+patch[0]+'-'+patch[-1]+'/'
 
-    fixed_config = {'WEIGHT_IMAGE': 'wts_bad.fits',
-                    'WEIGHT_TYPE' : 'MAP_WEIGHT',
-                    'FILTER_NAME' : 'gauss_5_31x31.conv'}
+    fixed_config = {'PARAMETERS_NAME' : 'myparams',
+                    'WEIGHT_IMAGE'    : 'wts_bad.fits',
+                    'WEIGHT_TYPE'     : 'MAP_WEIGHT',
+                    'FILTER_NAME'     : 'gauss_5_31x31.conv'}
 
     sw = sexpy.SexWrapper(fixed_config)
             
+    print('**** testing influcence of DETECT_THRESH ****')
     detect_thresh = [2.0, 1.5, 1.0, 0.9, 0.8]
-    for dt in detect_thresh:
-        sw.set_config('DETECT_THRESH', dt)
-        cat = 'DETECT_THRESH_'+str(dt)+'.cat'
+    for thresh in detect_thresh:
+        testparam = 'DETECT_THRESH'
+        sw.set_config(testparam, thresh)
+        prefix = testparam+'_'+str(thresh)+'_'
+        sw.set_check_images('a', prefix=prefix)
+        cat = testparam+'_'+str(thresh)+'.cat'
         sw.run('img.fits', cat=cat, relpath=relpath)
+    sw.reset_config()
 
+    print('**** testing influcence of DETECT_MINAREA ****')
+    detect_minarea = [3, 5, 10, 50, 100, 500]
+    for min_area in detect_minarea:
+        testparam = 'DETECT_MINAREA'
+        sw.set_config(testparam, min_area)
+        prefix = testparam+'_'+str(min_area)+'_'
+        sw.set_check_images('a', prefix=prefix)
+        cat = testparam+'_'+str(min_area)+'.cat'
+        sw.run('img.fits', cat=cat, relpath=relpath)
+    sw.reset_config()
+
+    print('**** testing influcence of DEBLEND_NTHRESH ****')
+    deblend_nthresh = [1, 2, 8, 16, 32]
+    for n_thresh in deblend_nthresh:
+        testparam = 'DEBLEND_NTHRESH'
+        sw.set_config(testparam, n_thresh)
+        prefix = testparam+'_'+str(n_thresh)+'_'
+        sw.set_check_images('a', prefix=prefix)
+        cat = testparam+'_'+str(n_thresh)+'.cat'
+        sw.run('img.fits', cat=cat, relpath=relpath)
+    sw.reset_config()
+
+    print('**** test influcence of DEBLEND_MINCONT ****')
+    deblend_mincont = [0.01, 0.005, 0.0005, 0.00005]
+    for min_cont in deblend_mincont:
+        testparam = 'DEBLEND_MINCONT'
+        sw.set_config(testparam, min_cont)
+        prefix = testparam+'_'+str(min_cont)+'_'
+        sw.set_check_images('a', prefix=prefix)
+        cat = testparam+'_'+str(min_cont)+'.cat'
+        sw.run('img.fits', cat=cat, relpath=relpath)
     sw.reset_config()
     sw.write_config('../sexout/'+relpath+'fixed_config.sex')
 
