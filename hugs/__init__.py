7e809c7620a4d407ebcbaafbeae12cdf5f711865
Johnny Greco <jgreco@astro.princeton.edu>
2016-08-02 18:13:34 -0400

    started building pipeline

--- hugs/__init__.py
+++ hugs/__init__.py
@@ -1,1 +1,3 @@
-from .cutter import *
+from ._cutter import *
+from .pipe_steps import pipe_steps
+from . import parser
--- hugs/parser.py
+++ hugs/parser.py
@@ -1,8 +1,15 @@
-import argparse
 
-def parse_args():
+__all__ = ['parse_args']
+
+def parse_args(relpath=False):
+    import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('tract', type=str, help='HSC tract')
     parser.add_argument('patch', type=str, help='HSC patch')
+    parser.add_argument('-b', '--band', help='HSC band', default='I')
     args = parser.parse_args()
-    return args
+    if relpath:
+        tract, patch, band = args.tract, args.patch, args.band.upper()
+        return 'HSC-'+band+'/'+str(tract)+'/'+patch[0]+'-'+patch[-1]
+    else:
+        return args
