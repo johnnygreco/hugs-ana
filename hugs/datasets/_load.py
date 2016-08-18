647c7ee7711c3e55b32fd3861150e33565b72fff
Johnny Greco <jgreco@astro.princeton.edu>
2016-08-18 17:53:39 -0400

    added function to get yang bcgs

--- hugs/datasets/_load.py
+++ hugs/datasets/_load.py
@@ -6,7 +6,8 @@ import numpy as np
 from astropy.table import Table
 yangdir = os.path.join(os.environ.get('DATA_DIR'), 'catalogs/Yang')
 
-__all__ = ['load_yang_groups', 'load_yang_gals', 'get_group_prop']
+__all__ = ['load_yang_groups', 'load_yang_gals',
+           'load_yang_bcgs', 'get_group_prop']
 
 def load_yang_groups():
     """
@@ -17,7 +18,6 @@ def load_yang_groups():
     groups = Table([groups[:,i] for i in range(groups.shape[1])],
                    names=('group_id', 'ra', 'dec', 'z', 'Mh_Lest', 'Mh_Mest'),
                    dtype=(int, 'f8', 'f8', 'f8', 'f8', 'f8'))
-
     return groups
 
 
@@ -32,6 +32,19 @@ def load_yang_gals():
     members.remove_column('vagc_id')
     members.sort('group_id')
     return members
+
+
+def load_yang_bcgs():
+    """
+    Return an astropy table of parameters for BCGs
+    Yang groups. The table is sorted by group id.
+    """
+    fn = os.path.join(yangdir, 'yang_modelC_brightest.txt')
+    bcgs = Table.read(fn, format='ascii')
+    bcgs.remove_column('gal_id')
+    bcgs.remove_column('vagc_id')
+    bcgs.sort('group_id')
+    return bcgs
 
 
 def get_group_prop(group_id, keys):
