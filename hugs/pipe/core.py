6e715a9f8b54c239e5e35112f83901c2c4061da2
Johnny Greco <jgreco@astro.princeton.edu>
2016-10-04 14:09:12 -0400

    Associate function added

--- hugs/pipe/core.py
+++ hugs/pipe/core.py
@@ -1,9 +1,10 @@
 from __future__ import divison, print_function
 
+import numpy as np
 
 import lsst.afw.detection as afwDetect
 
-__all__ = ['thresh_type', 'get_psf_sigma', 'image_threshold']
+__all__ = ['get_psf_sigma', 'associate', 'image_threshold']
 
 
 def get_psf_sigma(exposure):
@@ -26,9 +27,58 @@ def get_psf_sigma(exposure):
     return sigma
 
 
+def associate(mask, fpset, radius=10, plane_name='THRESH_HIGH'):
+    """
+    Associate footprints in fpset with footprints in mask plane 
+    'plane_name'. A footprint is associated with an object if 
+    its center is within 'radius' pixels from an on bit in this 
+    plane.
+
+    Parameters
+    ----------
+    mask : lsst.afw.image.imageLib.MaskU
+        Mask object with plane named 'plane_name'.
+    fpset : lsst.afw.detection.detectionLib.FootprintSet
+        Set of footprints to associate with objects in mask.
+    radius : float
+        Radius of association (in pixels) from the center of each
+        footprint in fpset.
+    plane_name : string
+        Name of the bit plane in mask to associate footprints with. 
+
+    Returns
+    -------
+    seg_assoc : 2D ndarray
+        Segmentation image with non-zero values for all footprints 
+        that are associated with an object in the mask. 
+
+    Notes
+    -----
+    seg_assoc also includes footprints near objects with the 
+    'BRIGHT_OBJECT' bit set.
+    """
+    from skimage.draw import circle 
+
+    xy0 = mask.getXY0()
+    bit_assoc = mask.getPlaneBitMask(plane_name)
+    bit_bright = mask.getPlaneBitMask('BRIGHT_OBJECT')
+    bit_vals = 2**np.arange(mask.getNumPlanesUsed())
+    # False --> use footprint ids
+    seg_assoc = fpset.insertIntoImage(False).getArray().copy() 
+    for foot in fpset.getFootprints():
+        peaks = np.array([p.getCentroid()-xy0 for p in foot.getPeaks()])
+        xc, yc = peaks.mean(axis=0)
+        rows, cols = circle(yc, xc, radius, shape=mask.getArray().shape)
+        circ_pix = mask.getArray()[rows, cols]
+        on_bits = [(bit_assoc in bit_vals[(b & bit_vals)!=0]) or\
+                   (bit_bright in bit_vals[(b & bit_vals)!=0]) for b in circ_pix]
+        if np.sum(on_bits)==0:
+            seg_assoc[seg_assoc==foot.getId()] = 0
+    return seg_assoc
+
+
 def image_threshold(masked_image, thresh, thresh_type='stdev', npix=1, 
-                    rgrow=None, isogrow=False, plane_name='DETECTED', 
-                    clear_mask=True):
+                    rgrow=None, isogrow=False, plane_name='', clear_mask=True):
     """
     Image thresholding. As bit mask will be set with name 'plane_name'.
 
@@ -49,7 +99,7 @@ def image_threshold(masked_image, thresh, thresh_type='stdev', npix=1,
         If True, use (expensive) isotropic grow. 
     plane_name : string, optional
         Name of bit plane.
-    clear_plane : bool, optional
+    clear_mask : bool, optional
         If True, clear the bit plane before thresholding
 
     Returns
@@ -58,14 +108,16 @@ def image_threshold(masked_image, thresh, thresh_type='stdev', npix=1,
         Footprints assoicated with detected objects.
     """
     mask = masked_image.getMask()
-    if clear_mask:
-        mask.clearMaskPlane(mask.getMaskPlane(mask_name))
     thresh_type = getattr(afwDetect.Threshold, thresh_type.upper())
     thresh = afwDetect.Threshold(thresh, thresh_type)
-    fp = afwDetect.FootprintSet(masked_image, thresh, mask_name, npix)
-    if ngrow is not None:
+    fp = afwDetect.FootprintSet(masked_image, thresh, npix)
+    if rgrow is not None:
         fp = afwDetect.FootprintSet(fp, rgrow, isogrow)
-    fp.setMask(mask, mask_name)
+    if plane_name:
+        if clear_mask:
+            mask.clearMaskPlane(mask.getMaskPlane(plane_name))
+        mask.addMaskPlane(plane_name)
+        fp.setMask(mask, plane_name)
     return fp
 
 
