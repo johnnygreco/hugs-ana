1d40ca537cac24cbfa643507fd8255fa3c09ba82
Johnny Greco <jgreco@astro.princeton.edu>
2016-10-23 20:52:16 -0400

    added mass color scale

--- plots/group_sample.py
+++ plots/group_sample.py
@@ -14,7 +14,9 @@ from toolbox.cosmo import Cosmology
 from toolbox.utils.plotting import line_widths
 import hugs
 cmap = plt.cm.rainbow
-plt.style.use('seaborn-talk')
+#plt.style.use('ggplot')
+plt.style.use('jpg')
+plt.rc('text', usetex=False)
 cosmo = Cosmology()
 
 datadir = '../results/'
@@ -42,7 +44,7 @@ f2, a2 = plt.subplots(5,1, figsize=(10, 13))
 f2.subplots_adjust(hspace=0.1, right=0.8)
 a2 = a2.flatten()
 
-ralim = [[223, 253], [125, 155], [165, 195], [200, 230], [325, 355]] 
+ralim = [[223, 253], [123, 153], [163, 193], [203, 233], [328, 358]] 
 declim = [[40, 47], [-2, 5], [-4, 3], [-3.5, 3.5], [-2, 5]]
 
 # norm is a class which, when called, can normalize data into the
@@ -53,6 +55,20 @@ c_m = plt.cm.rainbow
 # create a ScalarMappable and initialize a data structure
 s_m = plt.cm.ScalarMappable(cmap=c_m, norm=norm)
 s_m.set_array([])
+
+norm2 = plt.Normalize(vmin=groups['Mh_Lest'].min(), 
+                      vmax=groups['Mh_Lest'].max())
+
+s_m2 = plt.cm.ScalarMappable(cmap=plt.cm.gnuplot, norm=norm2)
+s_m2.set_array([])
+
+a2[-1].set_xlabel(r'$\alpha$ [deg]')
+cbar_ax = f2.add_axes([0.84, 0.23, 0.03, 0.25])
+f2.colorbar(s_m, cax=cbar_ax)
+cbar_ax.set_ylabel(r'$z$', fontsize=1)
+cbar_ax2 = f2.add_axes([0.84, 0.51, 0.03, 0.25])
+f2.colorbar(s_m2, cax=cbar_ax2)
+cbar_ax2.set_ylabel(r'$\log_{10}M_h/M_\odot$')
 
 for i in range(len(a2)):
     a2[i].set_aspect('equal')
@@ -71,18 +87,15 @@ for i in range(len(a2)):
             g['ra'], g['dec'], 2*g['theta_180'], steps=30)
         ra, dec = list(cone.to_radec())[0]
         ra = ra + 360*(ra<0)
-        a2[i].plot(ra, dec, c=s_m.to_rgba(g['z']), lw=1.8)
-    a2[i].scatter(groups[cut]['ra'], groups[cut]['dec'], color='k', alpha=0.7)
+        a2[i].plot(ra, dec, c=s_m.to_rgba(g['z']), lw=2.0)
+    a2[i].scatter(groups[cut]['ra'], groups[cut]['dec'], s=50,
+                  color=s_m2.to_rgba(groups[cut]['Mh_Lest']))
     a2[i].set_xlim(ralim[i])
     a2[i].set_ylim(declim[i])
     a2[i].minorticks_on()
-    a2[i].tick_params(labelsize=12)
+    a2[i].tick_params(axis='y', labelsize=16)
     a2[i].set_ylabel(r'$\delta$ [deg]')
 
-a2[-1].set_xlabel(r'$\alpha$ [deg]')
-cbar_ax = f2.add_axes([0.84, 0.28, 0.03, 0.4])
-f2.colorbar(s_m, cax=cbar_ax)
-cbar_ax.set_ylabel(r'$z$')
 f2.savefig('../figures/groups_2r180_cones.pdf', bbox_inches='tight')
 
 f3, a3 = plt.subplots()
@@ -90,6 +103,10 @@ for ra, dec in pointings['ra', 'dec']:
     diam = 1.5
     a3.add_patch(Ellipse((ra, dec), diam, diam, color='gray', alpha=0.05))
 a3.scatter(groups['ra'], groups['dec'], color='k', alpha=0.7)
+
+f4, a4 = plt.subplots()
+a4.hist(groups['Mh_Lest'], color='k')
+a4.set_xlabel(r'$\log_{10} M_h/M_\odot$')
 
 import RaiseWindow
 plt.show()
