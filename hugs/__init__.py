ae64c8079f33ba3bb57fba0d9fff6210fc429170
Johnny Greco <jgreco@astro.princeton.edu>
2017-02-13 17:00:49 -0500

    check for lsst stack so i can use python 3

--- hugs/__init__.py
+++ hugs/__init__.py
@@ -1,5 +1,13 @@
-from . import imtools
+from __future__ import division, print_function 
+
 from . import datasets
-from . import imfit
-from . import tasks
 from .utils import *
+from . import imtools
+
+try:
+    from . import imfit
+    from . import tasks
+except ImportError:
+    warn = ('Warning: must have lsst stack ' 
+           'installed to use the imfit and task modules')
+    print(warn)
